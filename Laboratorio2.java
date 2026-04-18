fc9cdf5edc06c2f7e44b91b0e2216abfc11623e7
Jmario19-bit <mc296550@gmail.com>
2026-04-18 13:30:08 -0600

    Add files via upload

---
+++ Laboratorio2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2;
+
+/**
+ *
+ * @author mc296
+ */
+public class Laboratorio2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
